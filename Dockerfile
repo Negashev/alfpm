2e6c131e60ebbe923c7987cbfd7027b010a7a2c7
negashev.al <aleksandr.negashev@domru.ru>
2017-10-31 11:02:05 +0500

    add apscheduler to collect data

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ WORKDIR /code
 
 # build japronto
 RUN apk add --no-cache python3 ca-certificates
-RUN apk add --no-cache --virtual .build-deps build-base py3-pip \
-    && pip3 install requests prometheus-client \
+RUN apk add --no-cache --virtual .build-deps py3-pip \
+    && pip3 install requests apscheduler prometheus-client \
     && apk del .build-deps \
     && rm -rf /var/cache/apk/*
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import os
 
 import re
@@ -8,11 +9,14 @@ from prometheus_client.parser import text_string_to_metric_families
 from wsgiref.simple_server import make_server
 
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
+from apscheduler.schedulers.background import BackgroundScheduler
 
 url = os.getenv('METRIC_HOST', 'http://localhost')
 path = os.getenv('METRIC_PATH', 'metrics')
 port = int(os.getenv('METRIC_PORT', 8080))
 source_label = os.getenv('SOURCE_LABEL')
+url_request = f'{url}:{port}/{path}'
+collected_dict = {}
 
 
 def hashFor(data):
@@ -24,41 +28,51 @@ def hashFor(data):
     return hashId.hexdigest()
 
 
+def collect():
+    global collected_dict
+    metricsFamily = {}
+    for i in text_string_to_metric_families(requests.get(url_request).text):
+        if i.samples[0][0] in ['process_cpu_seconds_total',
+                               'process_virtual_memory_bytes',
+                               'process_resident_memory_bytes',
+                               'process_start_time_seconds',
+                               'process_max_fds',
+                               'process_open_fds',
+                               'process_fake_namespace']:
+            continue
+        for item in i.samples:
+            find_source = '*'
+            if source_label in item[1]:
+                find_source = item[1][source_label]
+                if os.environ.get('SOURCE_REGEXP') is not None:
+                    searched = re.search(os.getenv('SOURCE_REGEXP', '(.*)'), item[1][source_label], re.IGNORECASE)
+                    if searched:
+                        find_source = searched.group(1)
+
+            sourceKey = ['environment']
+            sourceValue = [find_source]
+            list_keys = list(item[1].keys()) + sourceKey
+            hash_list_keys = hashFor(list_keys)
+            if hash_list_keys not in metricsFamily.keys():
+                metricsFamily[hash_list_keys] = GaugeMetricFamily(item[0], i.documentation,
+                                                                  labels=list_keys)
+            metricsFamily[hash_list_keys].add_metric(list(item[1].values()) + sourceValue, value=item[2])
+    collected_dict = metricsFamily
+
+
 class CustomCollector(object):
     def collect(self):
-        metricsFamily = {}
-        for i in text_string_to_metric_families(requests.get(f'{url}:{port}/{path}').text):
-            if i.samples[0][0] in ['process_cpu_seconds_total',
-                                   'process_virtual_memory_bytes',
-                                   'process_resident_memory_bytes',
-                                   'process_start_time_seconds',
-                                   'process_max_fds',
-                                   'process_open_fds',
-                                   'process_fake_namespace']:
-                continue
-            for item in i.samples:
-                find_source = '*'
-                if source_label in item[1]:
-                    find_source = item[1][source_label]
-                    if os.environ.get('SOURCE_REGEXP') is not None:
-                        searched = re.search(os.getenv('SOURCE_REGEXP', '(.*)'), item[1][source_label], re.IGNORECASE)
-                        if searched:
-                            find_source = searched.group(1)
-
-                sourceKey = ['environment']
-                sourceValue = [find_source]
-                list_keys = list(item[1].keys()) + sourceKey
-                hash_list_keys = hashFor(list_keys)
-                if hash_list_keys not in metricsFamily.keys():
-                    metricsFamily[hash_list_keys] = GaugeMetricFamily(item[0], i.documentation,
-                                                                      labels=list_keys)
-                metricsFamily[hash_list_keys].add_metric(list(item[1].values()) + sourceValue, value=item[2])
-        for key in metricsFamily.keys():
-            yield metricsFamily[key]
+        global collected_dict
+        self.collected_dict = copy.deepcopy(collected_dict)
+        for key in self.collected_dict.keys():
+            yield self.collected_dict[key]
 
 
 REGISTRY.register(CustomCollector())
 
+scheduler = BackgroundScheduler(timezone="UTC")
+scheduler.add_job(collect, 'interval', seconds=int(os.getenv('COLLECT_INTERVAL', 10)), max_instances=1)
+scheduler.start()
 app = make_wsgi_app()
 httpd = make_server('0.0.0.0', 9101, app)
 httpd.serve_forever()
